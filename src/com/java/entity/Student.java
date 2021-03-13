d1499eca9a47f01ff46b1bec595abafdaa410f13
wjx <1363306384@qq.com>
2021-03-13 12:52:54 +0800

    3.13第er次commit

--- src/com/java/entity/Student.java
+++ src/com/java/entity/Student.java
@@ -5,7 +5,18 @@ public class Student {
 
     private String address;
 
-    public static void main(String[] args) {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name="冯鑫辉";
+        student.address="文水县";
+        System.out.println(student);
     }
 }
